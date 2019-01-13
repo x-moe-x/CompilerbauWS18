3bfa10f9605f16edcbbdffb5a67acf1edce9bdc6
Moritz Gericke <moritz.gericke@hhu.de>
2019-01-13 20:59:09 +0100

    implemented missing classes

--- src/calculator/interpreter/Interpreter.java
+++ src/calculator/interpreter/Interpreter.java
@@ -6,6 +6,28 @@ import calculator.node.AProgram;
 public class Interpreter extends DepthFirstAdapter {
 	@Override
 	public void caseAProgram(AProgram node) {
-		super.caseAProgram(node);
+		String l = node.getLeft().getText().trim();
+		String r = node.getRight().getText().trim();
+		String sign = node.getOperation().getText().trim();
+
+		double left = new Double(l);
+		double right = new Double(r);
+		double result = 0;
+
+		switch (sign) {
+			case ("+"):
+				result = left + right;
+				break;
+			case ("-"):
+				result = left - right;
+				break;
+			case ("*"):
+				result = left * right;
+				break;
+			case ("/"):
+				result = left / right;
+				break;
+		}
+		System.out.println(left + " " + sign + " " + right + " = " + result);
 	}
 }
--- src/calculator/runner/Main.java
+++ src/calculator/runner/Main.java
@@ -1,7 +1,27 @@
 package calculator.runner;
 
+import calculator.interpreter.Interpreter;
+import calculator.lexer.Lexer;
+import calculator.node.Start;
+import calculator.parser.Parser;
+
+import java.io.FileReader;
+import java.io.PushbackReader;
+
 public class Main {
 	public static void main(String[] args) {
-
+		if (args.length > 0) {
+			try {
+				Lexer lexer = new Lexer(new PushbackReader(new FileReader(args[0]), 1024));
+				Parser parser = new Parser(lexer);
+				Start ast = parser.parse();
+				Interpreter interpreter = new Interpreter();
+				ast.apply(interpreter);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		} else {
+			System.exit(1);
+		}
 	}
 }
